f7833c2bca666782df9fb31d7ab5c3dba6c29119
RedNixon <redNixon@protonmail.com>
2017-11-30 00:11:06 -0600

    Clasify'ing

--- softhsm_fun/softhsm_funcs.py
+++ softhsm_fun/softhsm_funcs.py
@@ -10,110 +10,145 @@ class Database():
     def __init__(self, dbName='hsm_fun.db'):
         self.connection = sqlite3.connect('hsmtest.db')
         self.cursor = self.connection.cursor()
-        self.cursor.execute('''CREATE TABLE if not exists keys (id integer primary key not null, label text,
-                            key text, iv text)''')
-        self.cursor.execute('''CREATE TABLE if not exists crypttex (id integer primary key not null,
-                            label text, data text, iv text, edata text)''')
+        self.cursor.execute('''CREATE TABLE if not exists
+                                keys (
+                                    id integer primary key not null,
+                                    label text,
+                                    key text)''')
+        self.cursor.execute('''CREATE TABLE if not exists
+                                crypttex (
+                                    id integer primary key not null,
+                                    label text,
+                                    cleartext text,
+                                    iv text,
+                                    ciphertext text)''')
         self.connection.commit()
 
 
-def getAESKey(session, label):
-        key = session.get_key(label=label)
-        return key
+class PKSession(object):
+    def __init__(self, label):
+        self.lib = pkcs11.lib('/usr/local/lib/softhsm/libsofthsm2.so')
+        self.token = self.lib.get_token(token_label=label)
+        super().__init__()
 
 
-def createRSAKey(db, token, label, store, printpub=False):
-    with token.open(rw=True, user_pin='1234') as session:
+class Key(PKSession):
+    def __init__(self):
+        super().__init__()
+
+    def get_key(self, label):
+            key = self.session.get_key(label=label)
+            return key
+
+    @classmethod
+    def new_key(self):
+        raise NotImplementedError
+
+
+class Crypt(object):
+    def __init__(self):
+        super().__init__()
+
+    def cryptData(self, db, token, data, label):
+        with token.open(rw=True, user_pin='1234') as session:
+            # Encrypt our data
+            key = self.get_key(session, label)
+            iv = session.generate_random(128)  # AES blocks are fixed at 128 bits
+            iv_encoded = base64.urlsafe_b64encode(iv)
+            crypttext = key.encrypt(data, mechanism_param=iv)
+            db.cursor.execute('insert into crypttex values(?,?,?,?,?)', (None, label, data, iv_encoded,
+                                                                         base64.urlsafe_b64encode(crypttext)))
+            db.connection.commit()
+            return crypttext, session
+
+    def decryptData(self, db, token, label):
+        with token.open(rw=True, user_pin='1234') as session:
+            # decrypt our data
+            iv_en, edata, data = db.cursor.execute('select iv, edata, data from crypttex where label=?',
+                                                   (label,)).fetchone()
+            key = self.get_key(session, label)
+            iv = base64.urlsafe_b64decode(iv_en)
+            cdata = base64.urlsafe_b64decode(edata)
+            crypttext = key.decrypt(cdata, mechanism_param=iv)
+            return crypttext
+
+
+class RSA(object):
+    def __init__(self):
+        super().__init__()
+
+    def new_key(self, label):
+        # Generate a RSA key
+        pub, priv = self.session.generate_keypair(pkcs11.KeyType.RSA, 4096, label=label, store=True)
+
+
+class AES(object):
+    def __init__(self):
+        super().__init__()
+
+    def new_key(self, label):
         # Generate an AES key in this session
-        pub, priv = session.generate_keypair(pkcs11.KeyType.RSA, 4096, label=label, store=True)
-        if printpub:
-            print(pub)
-
-
-def createAESKey(db, token, label, store):
-    with token.open(rw=True, user_pin='1234') as session:
-        # Generate an AES key in this session
-        key = session.generate_key(pkcs11.KeyType.AES, 256, store=store, label=label)
+        key = self.session.generate_key(pkcs11.KeyType.AES, 256, label=label)
         return key
-
-
-def cryptData(db, token, data, label):
-    with token.open(rw=True, user_pin='1234') as session:
-        # Encrypt our data
-        key = getAESKey(session, label)
-        iv = session.generate_random(128)  # AES blocks are fixed at 128 bits
-        iv_encoded = base64.urlsafe_b64encode(iv)
-        crypttext = key.encrypt(data, mechanism_param=iv)
-        db.cursor.execute('insert into crypttex values(?,?,?,?,?)', (None, label, data, iv_encoded,
-                                                                     base64.urlsafe_b64encode(crypttext)))
-        db.connection.commit()
-        return crypttext, session
-
-
-def decryptData(db, token, label):
-    with token.open(rw=True, user_pin='1234') as session:
-        # Encrypt our data
-        iv_en, edata, data = db.cursor.execute('select iv, edata, data from crypttex where label=?', (label,)
-                                               ).fetchone()
-        key = getAESKey(session, label)
-        iv = base64.urlsafe_b64decode(iv_en)
-        cdata = base64.urlsafe_b64decode(edata)
-
-        crypttext = key.decrypt(cdata, mechanism_param=iv)
-        print('clear: %s' % data)
-        print('crypted: %s' % crypttext)
-        print(crypttext == data)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='\
-    HSM scratchpad.')
-    parser.add_argument(
-        '--init-token', action='store_true', dest='init_token', default=False,
-        help='Create a new token.')
 
-    parser.add_argument(
-        '--gen-rsa', action='store_true', dest='gen_rsa', default=False,
-        help='Generate AES key.')
-    parser.add_argument(
+    # Common flags
+    common_parser = argparse.ArgumentParser(add_help=False)
+    common_parser.add_argument('--label', action="store")
+
+    # Encryption/Decryption operations
+    crypt_parser = argparse.ArgumentParser(add_help=False)
+    crypt_parser.add_argument(
+        '--iv', action='store', dest='iv', default=None,
+        help='Define the initialization vector.')
+    crypt_parser.add_argument(
         '--crypt', action='store_true', dest='crypt', default=False,
         help='Generate AES key.')
-    parser.add_argument(
+    crypt_parser.add_argument(
         '--decrypt', action='store_true', dest='decrypt', default=False,
         help='Generate AES key.')
-    parser.add_argument(
-        '--gen-aes', action='store_true', dest='gen_aes', default=False,
-        help='Generate AES key.')
-    parser.add_argument(
-        '--store', action='store_true', default=False,
-        help='Store the key.')
-    parser.add_argument(
-        '--slot', action='store', default=None,
-        help='Store the key.')
-    parser.add_argument(
-        '--label', action='store', dest='label', default=None, type=str,
-        help='Label for things')
-    parser.add_argument(
+    crypt_parser.add_argument(
         '--data', action='store', dest='data', default=None, type=str,
         help='Label for things')
-    args = parser.parse_args()
 
-    # Initialise our PKCS#11 librar
-    if args.init_token:
-        token = init_token(label=args.label)
-        print(token)
-        return
-    lib = pkcs11.lib('/usr/local/lib/softhsm/libsofthsm2.so')
-    token = lib.get_token(token_label=args.slot)
-    db = Database()
-    if args.gen_aes:
-        createAESKey(db, token, args.label, args.store)
-    if args.gen_rsa:
-        createRSAKey(db, token, args.label, args.store, True)
-    elif args.crypt:
-        cryptData(db, token, args.data, args.label)
-    elif args.decrypt:
-        decryptData(db, token, args.label)
+    # Key Operations
+    key_parser = argparse.ArgumentParser(add_help=False)
+    key_parser.add_argument(
+        '--new-key', action='store_true', dest='new_key', default=False,
+        help='Generate a new key.')
+    key_parser.add_argument(
+        '--store', action='store_true', default=False,
+        help='Store the key(if a new one was created).')
+
+    # Main Parser
+    parser = argparse.ArgumentParser(description='HSM scratchpad.', parents=[common_parser])
+    subparsers = parser.add_subparsers(help='commands')
+
+    # Token/Slot init fucntions
+    token_parser = subparsers.add_parser(
+        'init_slot', help='Initialize a new slot and token')
+    token_parser.add_argument(
+        '--user-pin', dest='user_pin', action='store', help='User Pin')
+    token_parser.add_argument(
+        '--so-pin', dest='so_pin', action='store', help='Security Officer Pin')
+    token_parser.add_argument(
+        '--slot', dest='slot', action='store', help='Optionally define a slot, defaults to --free')
+    token_parser.set_defaults(func=init_token)
+
+    # RSA functions
+    rsa_parser = subparsers.add_parser(
+        'rsa', help='RSA Functions', parents=[crypt_parser, key_parser])
+    rsa_parser.set_defaults(func=main)
+
+    # AES functions
+    aes_parser = subparsers.add_parser(
+        'aes', help='AES Functions', parents=[crypt_parser, key_parser])
+    aes_parser.set_defaults(func=main)
+
+    args = parser.parse_args()
+    args.func
 
 
 if __name__ == '__main__':
